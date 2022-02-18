04b5788caef77cb44187242a3d1fa6c7dcbc5f46
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-18 22:42:00 +0300

    add bootstrap and fontawesome

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
