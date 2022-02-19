1cc5b7aa5d0c0176a75dfadb2f833aa01a679c47
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-19 18:48:34 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAppleWhole } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
-function App() {
-	return <div className='App'>CONTACTS</div>;
-}
+const App = () => {
+	return (
+		<>
+			<button className='btn btn-success me-2 mt-2'>
+				<FontAwesomeIcon icon={faAppleWhole} />
+				hello
+			</button>
+			<button className='btn btn-success me-2'>hello</button>
+		</>
+	);
+};
 
 export default App;
