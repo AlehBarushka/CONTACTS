cbc47eb6130e335a132f8484296d916961781931
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-20 20:33:43 +0300

    cteated api to get all contacts data

---
+++ src/services/ContactService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export class ContactService {
+	static serverURL = `http://localhost:9000`;
+
+	static getAllContacts() {
+		let dataURL = `${this.serverURL}/contacts`;
+		return axios.get(dataURL);
+	}
+}
