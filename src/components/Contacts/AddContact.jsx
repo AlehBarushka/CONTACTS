5e0fa0987f9147f026aa651e61766dd12e322245
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-21 18:50:08 +0300

    added state, effect to post new contact and navigate after them to home page, added updateInput handler onChange, handlesubmit onSubmit form

--- src/components/Contacts/AddContact.jsx
+++ src/components/Contacts/AddContact.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ContactService } from '../../services/ContactService';
 
 const AddContact = () => {
+	const [state, setState] = useState({
+		isLoading: false,
+		contact: {
+			name: '',
+			photo: '',
+			mobile: '',
+			email: '',
+			company: '',
+			title: '',
+			groupId: '',
+		},
+		groups: [],
+		error: '',
+	});
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const getAllGoups = async () => {
+			try {
+				setState({ ...state, isLoading: true });
+				let response = await ContactService.getAllGoups();
+				setState({ ...state, isLoading: false, groups: response.data });
+			} catch (error) {
+				setState({ ...state, isLoading: false, error: error.message });
+			}
+		};
+		getAllGoups();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const updateInput = (e) => {
+		setState({
+			...state,
+			contact: {
+				...state.contact,
+				[e.target.name]: e.target.value,
+			},
+		});
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			let response = await ContactService.createContact(state.contact);
+			if (response) {
+				navigate('/contacts/list', { replace: true });
+			}
+		} catch (error) {
+			setState({ ...state, isLoading: false, error: error.message });
+			navigate('/contacts/add', { replace: false });
+		}
+	};
+
+	const { isLoading, contact, groups, error } = state;
+
 	return (
 		<>
 			<section className='add-contact p-3'>
@@ -19,9 +76,13 @@ const AddContact = () => {
 					</div>
 					<div className='row'>
 						<div className='col-md-4'>
-							<form>
+							<form onSubmit={handleSubmit}>
 								<div className='mb-2'>
 									<input
+										name='name'
+										required={true}
+										value={contact.name}
+										onChange={updateInput}
 										type='text'
 										className='form-control'
 										placeholder='Name'
@@ -29,6 +90,9 @@ const AddContact = () => {
 								</div>
 								<div className='mb-2'>
 									<input
+										name='photo'
+										value={contact.photo}
+										onChange={updateInput}
 										type='text'
 										className='form-control'
 										placeholder='Photo URL'
@@ -36,6 +100,10 @@ const AddContact = () => {
 								</div>
 								<div className='mb-2'>
 									<input
+										name='mobile'
+										required={true}
+										value={contact.mobile}
+										onChange={updateInput}
 										type='number'
 										className='form-control'
 										placeholder='Mobile number'
@@ -43,6 +111,10 @@ const AddContact = () => {
 								</div>
 								<div className='mb-2'>
 									<input
+										name='email'
+										required={true}
+										value={contact.email}
+										onChange={updateInput}
 										type='email'
 										className='form-control'
 										placeholder='Email'
@@ -50,6 +122,10 @@ const AddContact = () => {
 								</div>
 								<div className='mb-2'>
 									<input
+										name='company'
+										required={true}
+										value={contact.company}
+										onChange={updateInput}
 										type='text'
 										className='form-control'
 										placeholder='Company'
@@ -57,14 +133,32 @@ const AddContact = () => {
 								</div>
 								<div className='mb-2'>
 									<input
+										name='title'
+										required={true}
+										value={contact.title}
+										onChange={updateInput}
 										type='text'
 										className='form-control'
 										placeholder='Title'
 									/>
 								</div>
 								<div className='mb-2'>
-									<select className='form-control'>
+									<select
+										name='groupId'
+										required={true}
+										value={contact.groupId}
+										onChange={updateInput}
+										className='form-control'
+									>
 										<option value=''>Select a group</option>
+										{groups.length > 0 &&
+											groups.map((group) => {
+												return (
+													<option key={group.id} value={group.id}>
+														{group.name}
+													</option>
+												);
+											})}
 									</select>
 								</div>
 								<div className='mb-2'>
