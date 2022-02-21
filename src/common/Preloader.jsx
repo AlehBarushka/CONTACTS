7476ec6d9adf4d4ab48a440f9123affe664b6c27
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-21 13:21:58 +0300

    added preloader spiner

---
+++ src/common/Preloader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import loader from '../assets/loader.svg';
+
+const Preloader = () => {
+	return (
+		<img
+			className='d-block m-auto'
+			src={loader}
+			alt='loader'
+			style={{ width: '150px' }}
+		/>
+	);
+};
+
+export default Preloader;
