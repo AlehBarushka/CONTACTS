f092021040fe19c278577a63a3566a63ec847a96
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-21 13:22:58 +0300

    added getGroup

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -7,4 +7,20 @@ export class ContactService {
 		let dataURL = `${this.serverURL}/contacts`;
 		return axios.get(dataURL);
 	}
+
+	static getContact(id) {
+		let dataURL = `${this.serverURL}/contacts/${id}`;
+		return axios.get(dataURL);
+	}
+
+	static getAllGoups() {
+		let dataURL = `${this.serverURL}/groups`;
+		return axios.get(dataURL);
+	}
+
+	static getGroup(contact) {
+		let groupId = contact.groupId;
+		let dataURL = `${this.serverURL}/groups/${groupId}`;
+		return axios.get(dataURL);
+	}
 }
