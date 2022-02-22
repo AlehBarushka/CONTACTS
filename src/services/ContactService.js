2d760219e0a325bb848378c536eee29e7eb3d2a4
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-22 12:53:41 +0300

    added update and delete contact methods

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -28,4 +28,14 @@ export class ContactService {
 		let dataURL = `${this.serverURL}/contacts`;
 		return axios.post(dataURL, contact);
 	}
+
+	static updateContact(contact, contastId) {
+		let dataURL = `${this.serverURL}/contacts/${contastId}`;
+		return axios.put(dataURL, contact);
+	}
+
+	static deleteContact(contactId) {
+		let dataURL = `${this.serverURL}/contacts/${contactId}`;
+		return axios.delete(dataURL);
+	}
 }
