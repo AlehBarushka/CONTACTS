d1c7345cfb3ea50b414ae2cdb4d8999f08cde803
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-03-04 11:58:53 +0300

    configured store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import contactReducer from '../slices/contactSlice';
 
-const store = configureStore({});
+const store = configureStore({
+	reducer: {
+		contactsData: contactReducer,
+	},
+});
 
 export default store;
