f6d337349fc77207a85f462f73cdf0edaf12a80b
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-03-10 13:11:09 +0300

    dispatched actions

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { logIn } from '../../slices/authSlice';
+
 import LoginForm from './LoginForm';
 import Title from '../Contacts/Title';
 
 const Login = () => {
+	const dispatch = useDispatch();
+
+	const logInUser = (userData) => {
+		dispatch(logIn(userData));
+	};
+
 	return (
 		<>
 			<Title textColor='text-dark'>Login</Title>
 			<div className='container mt-3'>
 				<div className='col-lg-4 col-md-6'>
-					<LoginForm />
+					<LoginForm logInUser={logInUser} />
 				</div>
 			</div>
 		</>
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { signUp } from '../../slices/authSlice';
+
 import Title from '../Contacts/Title';
 import SignUpForm from './SignUpForm';
 
 const SignUp = () => {
+	const dispatch = useDispatch();
+
+	const registerNewUser = (userData) => {
+		dispatch(signUp(userData));
+	};
+
 	return (
 		<>
 			<Title textColor='text-dark'>SignUp</Title>
 			<div className='container mt-3'>
 				<div className='col-lg-4 col-md-6'>
-					<SignUpForm />
+					<SignUpForm registerNewUser={registerNewUser} />
 				</div>
 			</div>
 		</>
