93723130b19b8d73e9414d47daa8297e5d09c6d4
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-03-10 14:38:48 +0300

    added button display depending on authentication

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMobilePhone } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+	const isAuth = useSelector((state) => state.authData.isAuth);
+
 	return (
 		<>
 			<nav className='navbar navbar-dark bg-dark navbar-expand-sm'>
@@ -19,15 +22,26 @@ const Navbar = () => {
 						</Link>
 					</div>
 					<div className='col d-flex justify-content-end'>
-						<button className='btn btn-sm btn-outline-light me-1'>
-							Logout
-						</button>
-						<Link to={'/login'} className='btn btn-sm btn-outline-light me-1'>
-							Login
-						</Link>
-						<Link to={'/signup'} className='btn btn-sm btn-outline-light me-1'>
-							SignUp
-						</Link>
+						{!isAuth ? (
+							<>
+								<Link
+									to={'/login'}
+									className='btn btn-sm btn-outline-light me-1'
+								>
+									Login
+								</Link>
+								<Link
+									to={'/signup'}
+									className='btn btn-sm btn-outline-light me-1'
+								>
+									SignUp
+								</Link>
+							</>
+						) : (
+							<button className='btn btn-sm btn-outline-light me-1'>
+								Logout
+							</button>
+						)}
 					</div>
 				</div>
 			</nav>
