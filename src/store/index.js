09b34e059cac77e6317d884d41e2860c58ff07ec
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-03-10 13:08:38 +0300

    added authReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactReducer from '../slices/contactSlice';
+import authReducer from '../slices/authSlice';
 
 const store = configureStore({
 	reducer: {
 		contactsData: contactReducer,
+		authData: authReducer,
 	},
 });
 
