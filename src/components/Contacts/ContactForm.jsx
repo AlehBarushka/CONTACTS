d41f79c6a9bc031dbbbf44262e5172723684c613
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-03-12 13:49:56 +0300

    added react-bootstrap form component with UI validation, added validation schema from 'yup', destructurization of props

--- src/components/Contacts/ContactForm.jsx
+++ src/components/Contacts/ContactForm.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
 
-const ContactForm = (props) => {
-	const { onSubmitForm, groups, btnColor, contactData } = props;
+import * as Yup from 'yup';
 
+import { Button, Form } from 'react-bootstrap';
+
+const ContactForm = ({
+	onSubmitForm,
+	groups,
+	btnColor,
+	contactData,
+	children,
+}) => {
+	const contactFormSchema = Yup.object().shape({
+		name: Yup.string().required('Required'),
+		mobile: Yup.number().required('Required'),
+	});
 	const formik = useFormik({
 		initialValues: {
 			name: contactData.name,
@@ -15,86 +27,90 @@ const ContactForm = (props) => {
 			groupId: contactData.groupId,
 		},
 		onSubmit: onSubmitForm,
+		validationSchema: contactFormSchema,
 	});
+
 	return (
-		<form onSubmit={formik.handleSubmit}>
-			<div className='mb-2'>
-				<input
-					name='name'
-					value={formik.values.name}
-					onChange={formik.handleChange}
-					type='text'
-					className='form-control'
-					placeholder='Name'
-				/>
-			</div>
-			<div className='mb-2'>
-				<input
-					name='mobile'
-					value={formik.values.mobile}
-					onChange={formik.handleChange}
-					type='number'
-					className='form-control'
-					placeholder='Mobile number'
-				/>
-			</div>
-			<div className='mb-2'>
-				<input
-					name='email'
-					value={formik.values.email}
-					onChange={formik.handleChange}
-					type='email'
-					className='form-control'
-					placeholder='Email'
-				/>
-			</div>
-			<div className='mb-2'>
-				<input
-					name='company'
-					value={formik.values.company}
-					onChange={formik.handleChange}
-					type='text'
-					className='form-control'
-					placeholder='Company'
-				/>
-			</div>
-			<div className='mb-2'>
-				<input
-					name='title'
-					value={formik.values.title}
-					onChange={formik.handleChange}
-					type='text'
-					className='form-control'
-					placeholder='Title'
-				/>
-			</div>
-			<div className='mb-2'>
-				<select
-					name='groupId'
-					value={formik.values.groupId}
-					onChange={formik.handleChange}
-					className='form-control'
-				>
-					<option value=''>Select a group</option>
-					{groups.length > 0 &&
-						groups.map((group) => {
-							return (
-								<option key={group.id} value={group.id}>
-									{group.name}
-								</option>
-							);
-						})}
-				</select>
-			</div>
-			<div className='mb-2'>
-				<button type='submit' className={`btn ${btnColor}`}>
-					{props.children}
-				</button>
-				<Link to={'/contacts/list'} className='btn btn-dark ms-2'>
+		<>
+			<Form noValidate onSubmit={formik.handleSubmit}>
+				<Form.Group className='mb-2'>
+					<Form.Control
+						name='name'
+						value={formik.values.name}
+						onChange={formik.handleChange}
+						type='text'
+						placeholder='Name'
+						isInvalid={formik.errors?.name && formik.touched?.name}
+					/>
+					<Form.Control.Feedback type='invalid'>
+						{formik.errors.name}
+					</Form.Control.Feedback>
+				</Form.Group>
+				<Form.Group className='mb-2'>
+					<Form.Control
+						name='mobile'
+						value={formik.values.mobile}
+						onChange={formik.handleChange}
+						type='number'
+						placeholder='Mobile number'
+						isInvalid={formik.errors?.mobile && formik.touched?.mobile}
+					/>
+					<Form.Control.Feedback type='invalid'>
+						{formik.errors.mobile}
+					</Form.Control.Feedback>
+				</Form.Group>
+				<Form.Group className='mb-2'>
+					<Form.Control
+						name='email'
+						value={formik.values.email}
+						onChange={formik.handleChange}
+						type='email'
+						placeholder='Email'
+					/>
+				</Form.Group>
+				<Form.Group className='mb-2'>
+					<Form.Control
+						name='company'
+						value={formik.values.company}
+						onChange={formik.handleChange}
+						type='text'
+						placeholder='Company'
+					/>
+				</Form.Group>
+				<Form.Group className='mb-2'>
+					<Form.Control
+						name='title'
+						value={formik.values.title}
+						onChange={formik.handleChange}
+						type='text'
+						placeholder='Title'
+					/>
+				</Form.Group>
+				<Form.Group className='mb-2'>
+					<Form.Select
+						name='groupId'
+						value={formik.values.groupId}
+						onChange={formik.handleChange}
+					>
+						<option value=''>Select a group</option>
+						{groups.length > 0 &&
+							groups.map((group) => {
+								return (
+									<option key={group.id} value={group.id}>
+										{group.name}
+									</option>
+								);
+							})}
+					</Form.Select>
+				</Form.Group>
+				<Button variant={btnColor} type='submit' className='me-2'>
+					{children}
+				</Button>
+				<Link to={'/contacts/list'} className='btn btn-dark'>
 					Close
 				</Link>
-			</div>
-		</form>
+			</Form>
+		</>
 	);
 };
 
