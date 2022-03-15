b7aac84e4011192ea631b0f217d1f4136de763ce
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-03-15 12:56:28 +0300

    deleted all methods

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,11 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import {
-	getAuth,
-	createUserWithEmailAndPassword,
-	updateProfile,
-	signInWithEmailAndPassword,
-	signOut,
-} from 'firebase/auth';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyB8Pjg3dB2acgXX5BqQFaxQcWHLoCLF2Tg',
@@ -16,20 +9,4 @@ const firebaseConfig = {
 	appId: '1:358469295730:web:167f87ffc89e2cc7f6aceb',
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
-export const firebaseAuth = {
-	registerNewUser(email, password) {
-		return createUserWithEmailAndPassword(auth, email, password);
-	},
-	updateUserName(userName) {
-		return updateProfile(auth.currentUser, { displayName: userName });
-	},
-	logIn(email, password) {
-		return signInWithEmailAndPassword(auth, email, password);
-	},
-	logOut() {
-		return signOut(auth);
-	},
-};
+export const app = initializeApp(firebaseConfig);
