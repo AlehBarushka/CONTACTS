56bc91505f7c723d32f34dcdcf1588d92c9a42c1
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-03-15 12:56:05 +0300

    added auth methods to services

---
+++ src/services/firebase/auth.js
@@ -0,0 +1,41 @@
+import { app } from '.';
+import {
+	createUserWithEmailAndPassword,
+	updateProfile,
+	signInWithEmailAndPassword,
+	signOut,
+	getAuth,
+} from 'firebase/auth';
+
+export const auth = getAuth(app);
+
+export const firebaseAuth = {
+	async registerNewUser(email, password, userName) {
+		//create user
+		const resData = await createUserWithEmailAndPassword(auth, email, password);
+		//add displayName
+		await updateProfile(auth.currentUser, {
+			displayName: userName,
+		});
+		//return object user
+		const user = {
+			id: resData.user.uid,
+			userName: resData.user.displayName,
+			email: resData.user.email,
+		};
+		return user;
+	},
+	async logIn(email, password) {
+		const resData = await signInWithEmailAndPassword(auth, email, password);
+		//return object user
+		const user = {
+			id: resData.user.uid,
+			userName: resData.user.displayName,
+			email: resData.user.email,
+		};
+		return user;
+	},
+	async logOut() {
+		await signOut(auth);
+	},
+};
