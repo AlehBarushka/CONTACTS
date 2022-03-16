530938dfc9a965f4394e73430774b37a79209e32
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-03-16 18:52:22 +0300

    update import

--- src/components/Contacts/EditContact.jsx
+++ src/components/Contacts/EditContact.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-	addCurrentGroup,
 	deleteCurrentContact,
 	getContact,
 	updateContact,
